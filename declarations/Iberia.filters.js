2cd8feb9f671988d35487c02b72b0474d8e7cc9e
Martin Ratinaud <martinratinaud@gmail.com>
2022-05-18 15:20:02 +0400

    Lint files

--- declarations/Iberia.filters.js
+++ declarations/Iberia.filters.js
@@ -1,27 +1,27 @@
 export function makePreviousVersionsLinksClickable(document) {
-    document.querySelectorAll("a[title][data-contenttype]").forEach(aElement => {
-        const modalTitle = aElement.getAttribute("title")
-        const contenttype = aElement.getAttribute("data-contenttype")
-        const contentorigin = aElement.getAttribute("data-contentorigin")
-        const contentid = aElement.getAttribute("data-contentid")
+  document.querySelectorAll('a[title][data-contenttype]').forEach(aElement => {
+    const modalTitle = aElement.getAttribute('title');
+    const contenttype = aElement.getAttribute('data-contenttype');
+    const contentorigin = aElement.getAttribute('data-contentorigin');
+    const contentid = aElement.getAttribute('data-contentid');
 
-        const params = new URLSearchParams()
-        params.set("pagename", "IB_SE/CacheControl6h/BrowserCache3600/Page/ShowModal");
-        params.set("locale", "fr")
-        params.set("market", "FR")
-        params.set("assettype", contenttype)
-        params.set("assetid", contentid)
-        params.set("origin", contentorigin)
-        params.set("modalTitle", modalTitle)
-        params.set("url", "#")
-        params.set("d", "")
+    const params = new URLSearchParams();
 
-        aElement.setAttribute("href", "https://www.iberia.com/cs/Satellite?" + params.toString());
-    })
+    params.set('pagename', 'IB_SE/CacheControl6h/BrowserCache3600/Page/ShowModal');
+    params.set('locale', 'fr');
+    params.set('market', 'FR');
+    params.set('assettype', contenttype);
+    params.set('assetid', contentid);
+    params.set('origin', contentorigin);
+    params.set('modalTitle', modalTitle);
+    params.set('url', '#');
+    params.set('d', '');
+
+    aElement.setAttribute('href', `https://www.iberia.com/cs/Satellite?${params.toString()}`);
+  });
 }
 
 export function removeEmptyLinks(document) {
-    Array.from(document.querySelectorAll('[href="#"]')).map((link) =>
-        link.removeAttribute("href")
-    );
+  Array.from(document.querySelectorAll('[href="#"]')).map(link =>
+    link.removeAttribute('href'));
 }
