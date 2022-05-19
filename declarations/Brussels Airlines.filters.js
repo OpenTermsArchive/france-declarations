baafef7f1fa5cc99b57c8e7982f51859563d6aaf
Clément Biron <clement.biron@gmail.com>
2022-05-19 13:55:49 +0200

    Lint files

--- declarations/Brussels Airlines.filters.js
+++ declarations/Brussels Airlines.filters.js
@@ -3,7 +3,7 @@
  * ie: remove #accordion-item-section--2055481788 on titles
  */
 export function removeAccordionLinks(document) {
-    document.querySelectorAll("[data-toggle=\"collapse\"]").forEach(aElement => {
-        aElement.removeAttribute("href")
-    })
+  document.querySelectorAll('[data-toggle="collapse"]').forEach(aElement => {
+    aElement.removeAttribute('href');
+  });
 }
