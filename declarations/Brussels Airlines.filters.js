7e1c37b5ed8e31388c86b4e4e898a073f4d95e28
Clément Biron <clement.biron@gmail.com>
2022-05-19 11:14:11 +0200

    Simplify declaration

--- declarations/Brussels Airlines.filters.js
+++ declarations/Brussels Airlines.filters.js
@@ -1,10 +1,9 @@
 /**
  * Remove all accordion links (exists on titles)
  * ie: remove #accordion-item-section--2055481788 on titles
- * @param { Document } document
  */
 export function removeAccordionLinks(document) {
-    document.querySelectorAll("[data-toggle=\"collapse\"]").forEach(/** @type { HTMLAnchorElement } */ aElement => {
+    document.querySelectorAll("[data-toggle=\"collapse\"]").forEach(aElement => {
         aElement.removeAttribute("href")
     })
 }
