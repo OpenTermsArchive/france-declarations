f9231419982fff242455dd7a4274c7708f6105e6
Clément Biron <clement.biron@gmail.com>
2022-05-19 10:23:26 +0200

    Improve function name and explanations

--- declarations/Galeries Lafayette.filters.js
+++ declarations/Galeries Lafayette.filters.js
@@ -1,12 +1,14 @@
 import { wrapLinks } from 'links-finder'
 
 /**
-* Because of the presence of a <p> inside <hx> there is unnecessary spaces in markdown
-* DOM Source:
-* <h2 class="delivery-title">
-* <a name="SCItemConditions2"><p>ARTICLE I. OBJET</p></a>
-* </h2>
-**/
+ * Because of the presence of a <p> inside <hx> there is unnecessary spaces in markdown
+ * DOM source:
+ * <h2 class="delivery-title">
+ *  <a name="SCItemConditions2">
+ *   <p>ARTICLE I. OBJET</p>
+ *  </a>
+ * </h2>
+ */
 export function removeTitlesWhiteSpaces(document) {
     document.querySelectorAll(".delivery-title").forEach(el => {
         if (el.textContent.trim().length) {
@@ -16,10 +18,14 @@ export function removeTitlesWhiteSpaces(document) {
 }
 
 /**
- * delete all `<sup>></sup>` (from some buttons "Retour en haut de la page")
- * @param { Document } document
+ * Delete all `<sup>></sup>` (from some buttons "Retour en haut de la page")
+ * DOM source:
+ * <div style="text-align: justify;">
+ *  <sup>&gt;</sup>&nbsp;
+ *  <a href="#top">Retour en haut de la page</a>
+ * </div>
  */
-export function deleteReturnToTheTop(document) {
+export function deleteReturnToTopOperator(document) {
     document.querySelectorAll("sup").forEach(el => {
         if (el.textContent === '>') {
             el.remove()
