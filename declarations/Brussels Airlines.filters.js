cd21356ae95caaaff342b9382d517b1617b786f6
Clément Biron <clement.biron@gmail.com>
2025-05-20 10:08:29 +0200

    Update engine (#859)
    * Update engine
    
    * Lint filters

--- declarations/Brussels Airlines.filters.js
+++ declarations/Brussels Airlines.filters.js
@@ -1,7 +1,5 @@
-/**
- * Remove all accordion links (exists on titles)
- * ie: remove #accordion-item-section--2055481788 on titles
- */
+// Remove all accordion links from titles
+// ie: remove #accordion-item-section--2055481788 on titles
 export function removeAccordionLinks(document) {
   document.querySelectorAll('[data-toggle="collapse"]').forEach(aElement => {
     aElement.removeAttribute('href');
--- declarations/Devred.filters.js
+++ declarations/Devred.filters.js
@@ -1,13 +1,11 @@
-/**
- * Alma image is blinking so we replace it with text
- * DOM source:
- * En 3x sans frais à partir de 100 € avec
- * <a href="https://getalma.eu/customers" target="_blank">
- *   <img src="data:image/png;base64,iVBOR..." alt="Paiement en 3x sans frais par carte bancaire" width="55">
- * </a>
- * Generated content:
- * En 3x sans frais à partir de 100 € avec Alma
- */
+// Alma image is blinking so we replace it with text
+// DOM source:
+// En 3x sans frais à partir de 100 € avec
+// <a href="https://getalma.eu/customers" target="_blank">
+// <img src="data:image/png;base64,iVBOR..." alt="Paiement en 3x sans frais par carte bancaire" width="55">
+// </a>
+// Generated content:
+// En 3x sans frais à partir de 100 € avec Alma
 export function replaceAlmaImageLink(document) {
   const newNode = document.createTextNode('Alma');
   const almaLink = document.querySelector('a[href="https://getalma.eu/customers"]');
