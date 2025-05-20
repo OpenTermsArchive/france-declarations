b0193b828a6bd6cbb1aaeccd01f1931b76c73e33
Clément Biron <clement.biron@gmail.com>
2025-05-20 09:56:09 +0200

    Lint filters

--- declarations/Galeries Lafayette.filters.js
+++ declarations/Galeries Lafayette.filters.js
@@ -1,12 +1,10 @@
-/**
- * Because of the presence of a <p> inside <hx> there is unnecessary spaces in markdown
- * DOM source:
- * <h2 class="delivery-title">
- *  <a name="SCItemConditions2">
- *   <p>ARTICLE I. OBJET</p>
- *  </a>
- * </h2>
- */
+// Because of the presence of a <p> inside <hx> there is unnecessary spaces in markdown
+// DOM source:
+// <h2 class="delivery-title">
+//  <a name="SCItemConditions2">
+//   <p>ARTICLE I. OBJET</p>
+//  </a>
+// </h2>
 export function removeTitlesWhiteSpaces(document) {
   document.querySelectorAll('.delivery-title').forEach(el => {
     if (el.textContent.trim().length) {
@@ -15,14 +13,12 @@ export function removeTitlesWhiteSpaces(document) {
   });
 }
 
-/**
- * Delete all `<sup>></sup>` (from some buttons "Retour en haut de la page")
- * DOM source:
- * <div style="text-align: justify;">
- *  <sup>&gt;</sup>&nbsp;
- *  <a href="#top">Retour en haut de la page</a>
- * </div>
- */
+// Delete all `<sup>></sup>` (from some buttons "Retour en haut de la page")
+// DOM source:
+// <div style="text-align: justify;">
+//  <sup>&gt;</sup>&nbsp;
+//  <a href="#top">Retour en haut de la page</a>
+// </div>
 export function deleteReturnToTopOperator(document) {
   document.querySelectorAll('sup').forEach(el => {
     if (el.textContent === '>') {
